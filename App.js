dbc9ae0ee2c117d66bead6f35065fa3334c20a03
Umar Asif <umarasif737@gmail.com>
2023-01-17 20:29:23 +0500

    Custom ffont used

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Home from "./screens/home";
+import * as Font from "expo-font";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadFont = async () => {
+      await Font.loadAsync({
+        "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+        "nunito-semiBold": require("./assets/fonts/Nunito-SemiBold.ttf"),
+        "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+      });
+      setFontLoaded(true);
+    };
+    loadFont();
+  }, []);
+
+  if (!fontLoaded) {
+    return null;
+  }
   return <Home />;
 }
 
