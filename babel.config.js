e0bb1060120208efae9e0d7dd23989a1a9f6f6f1
Umar Asif <umarasif737@gmail.com>
2023-01-19 00:14:36 +0500

    App fixed again

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,19 @@
-module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  plugins: [
-    ["react-native-reanimated/babel"],
-    // other plugins
-  ],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@": "./src",
+            assets: "./assets",
+          },
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
+  };
 };
+``;
