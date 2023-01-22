80f7fb0477abe3fff326a863a361a6ba3533d3e5
Umar Asif <umarasif737@gmail.com>
2023-01-22 19:48:30 +0500

    review details done

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,16 +1,40 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 import { GlobalStyles } from "../styles/globalStyles";
 import Card from "../shared/card";
 
 export default function ReviewDetails({ route }) {
+  const rating = route.params.rating;
+  const images = {
+    ratings: {
+      1: require("../assets/rating-1.png"),
+      2: require("../assets/rating-2.png"),
+      3: require("../assets/rating-3.png"),
+      4: require("../assets/rating-4.png"),
+      5: require("../assets/rating-5.png"),
+    },
+  };
   return (
     <View style={GlobalStyles.container}>
       <Card>
-        <Text>{route.params.title}</Text>
+        <Text style={GlobalStyles.titleText}>{route.params.title}</Text>
         <Text>{route.params.body}</Text>
-        <Text>{route.params.rating}</Text>
+        <View style={styles.rating}>
+          <Text>GameZone rating: </Text>
+          <Image source={images.ratings[rating]} />
+        </View>
       </Card>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: "#eee",
+  },
+});
