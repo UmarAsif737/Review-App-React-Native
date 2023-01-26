e9225f9a194efffdfa46533f34f1aa92250b423e
Umar Asif <umarasif737@gmail.com>
2023-01-26 14:24:46 +0500

    modal adjusted

--- screens/home.js
+++ screens/home.js
@@ -59,7 +59,7 @@ export default function Home({ navigation, route }) {
             style={{ ...styles.modalToggle, ...styles.modalClose }}
             onPress={() => setModalOpen(false)}
           />
-          <Text>Hello from the modaal :)</Text>
+          <Text>Hello from the modal :)</Text>
         </View>
       </Modal>
       <MaterialIcons
